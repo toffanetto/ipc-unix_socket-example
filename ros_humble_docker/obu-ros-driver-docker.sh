4289f6aeb12f89e8eea588a5c962e90e02bbd32a
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2025-05-01 11:11:04 -0300

    Update docker version in sh

--- ros_humble_docker/obu-ros-driver-docker.sh
+++ ros_humble_docker/obu-ros-driver-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # For production in target 
-docker run -it --rm --ipc=host --volume /tmp:/tmp --network host --name obu-ros-driver obu-ros-driver:V0.2-arm64
+docker run -it --rm --ipc=host --volume /tmp:/tmp --network host --name obu-ros-driver obu-ros-driver:V0.3-arm64
